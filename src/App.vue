40688207080ed8f51643bdcbbda226c1e73254d2
183Natasha <168049595+183Natasha@users.noreply.github.com>
2026-07-04 15:29:46 +0500

    создана директива v-focus

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,7 @@
 		error.value = null;
 		// console.log(data.value)
 	}
+
 </script>
 
 <template>
--- src/components/CitySelect.vue
+++ src/components/CitySelect.vue
@@ -1,63 +1,66 @@
 <script setup>
-import { onMounted, onWatcherCleanup, ref, watch, watchEffect } from 'vue';
-import AppButton from './Button/AppButton.vue';
-import IconLocation from './icons/IconLocation.vue';
-import AppInput from './AppInput.vue';
+	import { onMounted, onWatcherCleanup, ref, watch, watchEffect } from 'vue';
+	import AppButton from './Button/AppButton.vue';
+	import IconLocation from './icons/IconLocation.vue';
+	import AppInput from './AppInput.vue';
 
+	let isEdited = ref(false);
+	const city = ref('Moscow');
 
+	onMounted(() => {
+		emit('selectCity', city.value);
+	});
 
-let isEdited = ref(false)
-const city = ref('Moscow')
+	const emit = defineEmits({
+		selectCity(payload) {
+			return payload ? true : false;
+		},
+	});
 
-onMounted(()=>{
-    emit('selectCity', city.value)
-})
+	function select() {
+		isEdited.value = false;
+		emit('selectCity', city.value);
+	}
 
-const emit = defineEmits({
-    selectCity(payload) {
-        return payload ? true : false
-    }
-})
+	function edit() {
+		isEdited.value = true;
+	}
 
-function select() {
-    isEdited.value = false
-    emit('selectCity', city.value)
-}
-
-function edit() {
-    isEdited.value = true
-}
 
 </script>
 
 <template>
-    <div class="city-select">
-        <div v-if="isEdited" class="city-input">
-            <AppInput v-model="city" placeholder="Введите город" @keyup.enter="select()"></AppInput>
-            <AppButton @click="select()">
-                Сохранить.
-            </AppButton>
-        </div>
+	<div class="city-select">
+		<div
+			v-if="isEdited"
+			class="city-input"
+		>
+			<AppInput
+				v-model="city"
+                v-focus
+				placeholder="Введите город"
+				@keyup.enter="select()"
+			></AppInput>
+			<AppButton @click="select()"> Сохранить. </AppButton>
+		</div>
 
-        <AppButton v-if="!isEdited" @click="edit">
-            <IconLocation></IconLocation>
-            Изменить город
-        </AppButton>
-    </div>
-
-
-
-
+		<AppButton
+			v-if="!isEdited"
+			@click="edit"
+		>
+			<IconLocation></IconLocation>
+			Изменить город
+		</AppButton>
+	</div>
 </template>
 
 <style scoped>
-.city-input {
-    display: flex;
-    gap: 12px;
+	.city-input {
+		display: flex;
+		gap: 12px;
+	}
 
-}
-
-.city-select {
-    width: 420px;
-}
+	.city-select {
+		width: 420px;
+	}
 </style>
