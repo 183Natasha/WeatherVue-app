52f77328441d2d9f2ef799b1f0f41f96bd0578d7
183Natasha <168049595+183Natasha@users.noreply.github.com>
2026-07-05 22:29:44 +0500

    Рефакторинг компонент

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,12 @@
 <script setup>
-	import { ref, reactive, computed } from 'vue';
-	import ApppStat from './components/ApppStat.vue';
-	import CitySelect from './components/CitySelect.vue';
-	import Error from './components/Error.vue';
-	import DayCard from './components/DayCard.vue';
+	import { ref } from 'vue';
+	import PanelRight from './components/PanelRight.vue';
 
 	const API_ENDPOINT = 'https://api.weatherapi.com/v1';
-
-	let errorMap = new Map([[1006, 'Указанный город не найден']]);
-	const errorDisplay = computed(() => {
-		return errorMap.get(error?.value?.error.code);
-	});
 
 	let data = ref();
 	let error = ref();
 	let activeIndex = ref(0);
-
-	let deteModified = computed(() => {
-		if (!data.value) {
-			return [];
-		}
-		return [
-			{
-				label: 'Влажность',
-				stat: data.value.current.humidity + ' %',
-			},
-			{
-				label: 'Облачность',
-				stat: data.value.current.cloud + ' %',
-			},
-			{
-				label: 'Ветер',
-				stat: ((data.value.current.wind_kph * 1000) / 3600).toFixed(1) + ' м/с',
-			},
-		];
-	});
 
 	async function getCity(city) {
 		const params = new URLSearchParams({
@@ -61,33 +33,13 @@
 	<main class="main">
 		<div class="left"></div>
 		<div class="right">
-			<Error :error="errorDisplay"></Error>
-			<div
-				v-if="data"
-				class="stat-data"
-			>
-				<div class="stat-list">
-					<ApppStat
-						v-for="item in deteModified"
-						:key="item.label"
-						v-bind="item"
-					></ApppStat>
-				</div>
-
-				<div class="day-card-list">
-					<DayCard
-						v-for="(item, i) in data.forecast.forecastday"
-						:key="item.date"
-						:weatrer-code="item.day.condition.code"
-						:temp="item.day.avgtemp_c"
-						:date="new Date(item.date)"
-						:is-active="activeIndex == i"
-						@click="() => (activeIndex = i)"
-					></DayCard>
-				</div>
-			</div>
-
-			<CitySelect @select-city="getCity"></CitySelect>
+			<PanelRight
+				:data
+				:error
+				:active-index="activeIndex"
+				@select-index="(i) => (activeIndex = i)"
+				@select-city="getCity"
+			></PanelRight>
 		</div>
 	</main>
 </template>
@@ -103,30 +55,13 @@
 		padding: 60px 50px;
 		border-radius: 0 25px 25px 0;
 	}
-    .left{
-        width: 500px;
-        height: 680px;
-        border-radius: 30px;
-        background-image: url('public/bg.png');
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
-
-	.stat-list {
-		display: flex;
-		flex-direction: column;
-		gap: 16px;
+	.left {
+		width: 500px;
+		height: 680px;
+		border-radius: 30px;
+		background-image: url('public/bg.png');
+		background-repeat: no-repeat;
+		background-size: cover;
 	}
 
-	.stat-data {
-		display: flex;
-		flex-direction: column;
-		gap: 90px;
-	}
-
-	.day-card-list {
-		display: flex;
-		gap: 1px;
-		margin-bottom: 70px;
-	}
 </style>
